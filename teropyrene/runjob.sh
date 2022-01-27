131426c0aa6eb9b4c4a15c2232deeba7d2ec4249
lsyurievna <lsyurievna@mun.ca>
2022-01-27 11:27:06 -0330

    added files

---
+++ teropyrene/runjob.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --mem=32G             # memory, roughly 2 times %mem defined in the input name.com file
+#SBATCH --time=02-00:00       # expect run time (DD-HH:MM)
+#SBATCH --cpus-per-task=32    # No. of cpus for the job as defined by %nprocs in the name.com file
+#SBATCH --mail-user=lsyurievna@mun.ca
+#SBATCH --mail-type=ALL
+module load gaussian/g16.c01
+g16 < input.com >& teropyrene.log
